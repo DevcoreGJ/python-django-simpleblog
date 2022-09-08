89e29cb78008cc689c049c91a80f437503c5203c
nic ledger <nicledgerallmail@gmail.com>
2022-09-08 16:09:09 +0100

    django.view.generic import ListView, DetailView libreary added to views.py. Removed def home in views.py and replaced it with class HomeView, HomeView passes in ListView.

--- blogroot/views.py
+++ blogroot/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
+'''
 def home(request):
 	return render(request, 'home.html', {})
-	
+'''
+class HomeView(ListView): # pass in ListView
+	model = Post #so our post will appear as a list on homepage
+	template_name = ' home.html' #home html template already created
