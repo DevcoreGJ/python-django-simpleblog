c0cbbc990f69680d135cb4237ef0ff04a376cef3
nic ledger <nicledgerallmail@gmail.com>
2022-09-08 15:42:58 +0100

    made migrations so now can makes posts on the server.

--- blogroot/models.py
+++ blogroot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	title = models.CharField(max_length=255) #max char length
+	author = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted all related posts are
+	body = models.TextField() #input field for text
+
+	def __str__(self):
+		return self.title + ' | ' + self.author
