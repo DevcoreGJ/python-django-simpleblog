3cc36a568e5211efc3f08bc08fe50f030fbba6c1
nic ledger <nicledgerallmail@gmail.com>
2022-09-08 16:22:15 +0100

    home.html changed from hello world to and unordered lists pulling making requests from Post Model/Database, using a for loop to populate the list. In urls.py it changed from a direct url to an OOP url imported HomeView from views module.

--- blogroot/urls.py
+++ blogroot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
-
+from .views import HomeView
 urlpatterns = [
-    path('', views.home, name="home")
+    #path('', views.home, name="home") old type of url
+    path('', HomeView.as_view(), name="home"),
 ]
--- blogroot/views.py
+++ blogroot/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
+from .models import Post
 
 # Create your views here.
 '''
