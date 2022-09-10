b5b9df61ffd1e66deebd90c8877886b6fd9540c8
nic ledger <nicledgerallmail@gmail.com>
2022-09-10 16:42:39 +0100

    added a db field on models for custom titles for blog posts

--- blogroot/models.py
+++ blogroot/models.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
 	title = models.CharField(max_length=255) #max char length
+	title_tag = models.CharField(max_length=255, default = "please add title") #max char length
 	author = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted all related posts are
 	body = models.TextField() #input field for text
+	name = "Apollo Blog"
 
 	def __str__(self):
 		return self.title + ' | ' + str(self.author)
