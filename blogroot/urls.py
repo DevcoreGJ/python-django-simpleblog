14b1c19f5763e08c2e2356a850ac3b3bfe41d4d9
nic ledger <nicledgerallmail@gmail.com>
2022-09-15 16:16:07 +0100

    added an edit functionality.

--- blogroot/urls.py
+++ blogroot/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView 
 urlpatterns = [
     #path('', views.home, name="home") old type of url
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name='home'),
     #pk = primary key, the url will autogen a new page based n int
-    path('article/<int:pk>', ArticleDetailView.as_view(), name="article_detail"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
     
--- blogroot/views.py
+++ blogroot/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 
@@ -22,3 +22,8 @@ class AddPostView(CreateView):
 	template_name = 'add_post.html'
 	#fields = '__all__'
 	#fields = 'title', 'body'
+
+class UpdatePostView(UpdateView):
+	model = Post
+	template_name = 'update_post.html'
+	fields = ['title', 'title_tag', 'body']
