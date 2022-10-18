b180b73952cb5143ad24f11cd144054519cde0e0
nic ledger <nicledgerallmail@gmail.com>
2022-10-18 16:23:08 +0100

    added comment model. added comment retrieval on article_detail

--- blogroot/admin.py
+++ blogroot/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Category, Profile
+from .models import Post, Category, Profile, Comment
 
 admin.site.register(Post) # Posts are accessable in admin area
 admin.site.register(Category) # Posts are accessable in admin area
 admin.site.register(Profile) # Let's the added profile class in Models show in
 #djago admin area.
+
+admin.site.register(Comment) # Comment section will show up in Django admin area.
--- blogroot/models.py
+++ blogroot/models.py
@@ -62,4 +62,16 @@ class Post(models.Model):
 		#return reverse('article_detail', args=(str(self.id)) )
 		return reverse('home')
 
-	
+class Comment(models.Model):
+#post is the foreign key that connects the comment model to the post model.
+	post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+	#commenters name
+	name = models.CharField(max_length=255)
+	#what the content of the comment is
+	body = models.TextField()
+	#auto add date
+	date_added = models.DateTimeField(auto_now_add = True)
+
+	def __str__(self):
+		return '%s - %s' % (self.post.title, self.name)
+
